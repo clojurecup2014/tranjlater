520c652f3bfac6e50f22ad5aee6613bfb9a41aba
Rick Hall <rickhall2000@hotmail.com>
2014-09-28 06:53:20 -0400

    Make the signin form prettier

--- src-cljs/tranjlator/signin_view.cljs
+++ src-cljs/tranjlator/signin_view.cljs
@@ -22,13 +22,22 @@
     om/IRenderState
     (render-state [this {:keys [text] :as state}]
       (let [sender-ch (:sender-ch app)]
-        (dom/h3 #js {:className "container"}
-                (dom/div #js {:className "form-horizontal"}
-                         (dom/div #js {:className "form-group"}
-                                  (dom/input #js {:className "form-control" :type "text"
-                                                  :value text :id "user-name"
-                                                  :onKeyPress (fn [e] (check-for-enter e owner state app login-click))
-                                                  :onChange (fn [e] (text-entry e owner state))}))
-                         (dom/div #js {:className "col-xs-offset-2 col-xs-10"}
-                                  (dom/button #js {:type "button" :className "button"
-                                                   :onClick (fn [e] (login-click sender-ch text owner app))} "Join"))))))))
+        (dom/div nil
+                 (dom/div #js {:className "alert alert-info col-md-8"}
+                          (dom/p nil "Welcome to Tranjlator a chat room that lets you read the conversation translated into a language of your choosing.")
+                          (dom/p nil "Choose a screen name to get started."))
+                 (dom/div #js {:className "panel panel-default col-md-6 col-md-offset-1"}
+                          (dom/div #js {:className "panel-heading"}
+                                   (dom/div #js {:className "panel-title"}
+                                            (dom/h4 nil
+                                                    (dom/span #js {:className "glyphicon glyphicon-user"}) " Screen name")))
+                          (dom/div #js {:className "panel-body"}
+                                   (dom/div #js {:className "form"}
+                                            (dom/div nil
+                                                     (dom/input #js {:className "form-control col-md-8" :type "text"
+                                                                     :value text :id "user-name"
+                                                                     :onKeyPress (fn [e] (check-for-enter e owner state app login-click))
+                                                                     :onChange (fn [e] (text-entry e owner state))}))
+                                            (dom/div nil
+                                                     (dom/button #js {:type "button" :className "btn btn-primary col-md-3 col-md-offset-4"
+                                                                      :onClick (fn [e] (login-click sender-ch text owner app))} "Join"))))))))))

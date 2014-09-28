262b47b17a798338a118fbbcc953e07325ba559d
Rick Hall <rickhall2000@hotmail.com>
2014-09-28 09:43:47 -0400

    langs and topics strings and keywords

--- src-cljs/tranjlator/master_view.cljs
+++ src-cljs/tranjlator/master_view.cljs
@@ -116,7 +116,7 @@
                    (= :original topic) (om/transact! app :original (fn [col] (conj col msg)))
                    (= :user-join topic) (om/transact! app :users (fn [col] (conj col (:user-name msg []))))
                    (= :user-part topic) (om/transact! app :users (fn [col] (remove (fn [x] (= x (:user-name msg))) col)))
-                   (= lang topic) (om/transact! app :translated (fn [col] (conj col msg)))
+                   (= (str lang) (str topic)) (om/transact! app :translated (fn [col] (conj col msg)))
                    :default (println "RECVD:" msg "type: " (keys msg))))
                 (recur))))))
     om/IDidMount
